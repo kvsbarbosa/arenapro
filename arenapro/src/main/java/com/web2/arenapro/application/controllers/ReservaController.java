cfe4f5f34f4267e7731f349ec286b013ef6ae959
Kaio Vinícius <kaiovsbarbosa@gmail.com>
2024-11-27 21:18:45 -0300

    feat: controller

--- arenapro/src/main/java/com/web2/arenapro/application/controllers/ReservaController.java
+++ arenapro/src/main/java/com/web2/arenapro/application/controllers/ReservaController.java
@@ -1,9 +1,44 @@
 package com.web2.arenapro.application.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.web2.arenapro.application.services.ReservaService;
+import com.web2.arenapro.domain.dto.ReservaDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/reservas")
 public class ReservaController {
+
+    @Autowired
+    private ReservaService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ReservaDTO> findById(@PathVariable Long id) {
+        ReservaDTO reservaDTO = service.findById(id);
+        return ResponseEntity.ok(reservaDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservaDTO> save(@RequestBody ReservaDTO reservaDTO) {
+        reservaDTO = service.save(reservaDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(reservaDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(reservaDTO);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ReservaDTO> update(@PathVariable Long id, @RequestBody ReservaDTO reservaDTO) {
+        reservaDTO = service.update(id, reservaDTO);
+        return ResponseEntity.ok(reservaDTO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
