05078fff41aa09bfe71ca142a4dd8f940b7205b7
Rafael marinho <rafaelmarinhoalves5@gmail.com>
2024-11-29 10:50:43 -0300

    Delete arenapro/src/main directory

--- arenapro/src/main/java/com/web2/arenapro/application/services/exceptions/DatabaseException.java
+++
@@ -1,9 +0,0 @@
-package com.web2.arenapro.application.services.exceptions;
-
-public class DatabaseException extends RuntimeException {
-
-    public DatabaseException(String msg) {
-        super(msg);
-    }
-
-}
--- arenapro/src/main/java/com/web2/arenapro/application/services/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.web2.arenapro.application.services.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String msg) { super(msg); }
-
-}
